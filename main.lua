d676c53e929063132078eed0bc645f00270b7490
schazers <schazers@gmail.com>
2018-12-17 13:04:32 -0800

    better example
    - smaller bullets
    - bullets just from one direction
    - score counter
    - added collisions
    - game resets

--- main.lua
+++ main.lua
@@ -10,10 +10,28 @@ local ground = {}
 local player = {}
 local enemies = {}
 
-local ENEMY_LENGTH = 32
-local ENEMY_WIDTH = 8
-local ENEMY_HORIZONTAL_SPEED = 300
-local ENEMY_VERTICAL_SPEED = 400
+local ENEMY_WIDTH = 12
+local ENEMY_HEIGHT = ENEMY_WIDTH
+local ENEMY_SPEED = 300
+local ENEMY_SPAWN_RATE = 1.0
+
+local function resetGame()
+  num_enemies_cleared = 0
+
+  -- reset player
+  player.move_speed = 250
+  player.width = 32
+  player.height = 32
+  player.x = GAME_WIDTH / 2 + player.width / 2
+	player.y = GAME_HEIGHT / 2
+	player.y_velocity = -5
+  player.jump_initial_velocity = -600
+
+  -- clear all enemies
+  for enemy in pairs(enemies) do
+    enemies[enemy] = nil
+  end
+end
  
 function love.load()
   math.randomseed(os.time())
@@ -22,15 +40,9 @@ function love.load()
 	ground.height = 0.1 * GAME_HEIGHT
  
 	ground.x = 0
-	ground.y = GAME_HEIGHT - 0.1 * GAME_HEIGHT
- 
-  player.move_speed = 250
-  player.width = 32
-  player.height = 32
-  player.x = GAME_WIDTH / 2 + player.width / 2
-	player.y = GAME_HEIGHT / 2
-	player.y_velocity = -5
-  player.jump_initial_velocity = -600
+  ground.y = GAME_HEIGHT - 0.1 * GAME_HEIGHT
+  
+  resetGame()
 end
  
 function love.update(dt)
@@ -64,17 +76,23 @@ function love.update(dt)
 
   -- update existing enemies
   for i = 1, #enemies do
-    if enemies[i].x_velocity ~= 0 then
-      enemies[i].x = enemies[i].x + enemies[i].x_velocity * dt
-      if (enemies[i].x + ENEMY_LENGTH < 0) or (enemies[i].x > GAME_WIDTH) then
-        table.remove(enemies, i)
-        if i ~= #enemies then
-          i = i - 1
-        end
-      end
+    local enemy = enemies[i]
+    if enemy == nil then
+      -- do nothing: we're here because we already removed an enemy during this loop
     else
-      enemies[i].y = enemies[i].y + enemies[i].y_velocity * dt
-      if (enemies[i].y > ground.y) then
+      enemy.x = enemy.x + enemy.xVelocity * dt
+
+      -- handle collisions with player
+      if(enemy.x < player.x + player.width and 
+        enemy.x + ENEMY_WIDTH > player.x and
+        enemy.y < player.y + player.height and 
+        enemy.y + ENEMY_HEIGHT > player.y) then
+        resetGame()
+      end 
+      
+      -- remove enemies when they go offscreen
+      if (enemy.x < 0) then
+        num_enemies_cleared = num_enemies_cleared + 1
         table.remove(enemies, i)
         if i ~= #enemies then
           i = i - 1
@@ -83,35 +101,31 @@ function love.update(dt)
     end
   end
 
-  -- spawn enemies
-  if (math.random() < 1.0 * dt) then
+  -- spawn new enemies
+  -- TODO: don't allow any two bullets to spawn too close to each other in time?
+  local shouldSpawnEnemy = (math.random() < ENEMY_SPAWN_RATE * dt)
+  if (shouldSpawnEnemy) then
     local randFloat = math.random()
-    local enemy_velocity_x = 0
-    local enemy_velocity_y = 0
-    local enemy_x = 0
-    local enemy_y = 0
-    if (randFloat < 0.333) then
-      enemy_x = 0
-      enemy_y = ground.y - ENEMY_LENGTH
-      enemy_velocity_x = ENEMY_HORIZONTAL_SPEED
-    elseif (randFloat < 0.666) then
-      enemy_x = GAME_WIDTH
-      enemy_y = ground.y - (player.height / 2) - (ENEMY_LENGTH / 2)
-      enemy_velocity_x = -ENEMY_HORIZONTAL_SPEED
+    local enemyVelocityX = 0
+    local enemyX = 0
+    local enemyY = 0
+
+    if (randFloat < 0.5) then
+      enemyX = GAME_WIDTH
+      enemyY = ground.y - (player.height / 2) - (ENEMY_WIDTH / 2)
+      enemyVelocityX = -ENEMY_SPEED
     else
-      enemy_x = player.x
-      enemy_y = 0
-      enemy_velocity_y = ENEMY_VERTICAL_SPEED
+      enemyX = GAME_WIDTH
+      enemyY = ground.y - 2 * (player.height / 2) - (ENEMY_WIDTH / 2)
+      enemyVelocityX = -ENEMY_SPEED
     end
 
     enemies[#enemies + 1] = {
-      x = enemy_x, 
-      y = enemy_y,
-      x_velocity = enemy_velocity_x,
-      y_velocity = enemy_velocity_y
+      x = enemyX, 
+      y = enemyY,
+      xVelocity = enemyVelocityX
     }
   end
-
 end
  
 function love.draw()
@@ -121,34 +135,28 @@ function love.draw()
   gTranslateScreenToCenterDy = 0.5 * (love.graphics.getHeight() - GAME_HEIGHT)
   love.graphics.translate(gTranslateScreenToCenterDx, gTranslateScreenToCenterDy)
 
-  -- draw player
-  love.graphics.setColor(0.7, 0.25, 0.25, 1.0)
+  -- player
+  love.graphics.setColor(1.0, 0.4, 0.4, 1.0)
   love.graphics.rectangle('fill', player.x, player.y, player.width, player.height)
 
-  -- draw enemies
-  love.graphics.setColor(0.25, 0.7, 0.25, 1.0)
+  -- enemies
+  love.graphics.setColor(0.4, 1.0, 0.4, 1.0)
   for i = 1, #enemies do
-    local enemy = enemies[i]
-    local width = 0
-    local height = 0
-    if enemy.x_velocity ~= 0 then
-      width = ENEMY_LENGTH
-      height = ENEMY_WIDTH
-    else
-      width = ENEMY_WIDTH
-      height = ENEMY_LENGTH
-    end
-    love.graphics.rectangle('fill', enemy.x, enemy.y, width, height)
+    love.graphics.rectangle('fill', enemies[i].x, enemies[i].y, ENEMY_WIDTH, ENEMY_HEIGHT)
   end
 
-  -- draw ground
-  love.graphics.setColor(0.2, 0.2, 0.3, 1.0)
+  -- ground
+  love.graphics.setColor(0.3, 0.3, 0.5, 1.0)
   love.graphics.rectangle('fill', ground.x, ground.y, ground.width, ground.height)
 
-  -- draw frame
+  -- frame
   love.graphics.setColor(1.0, 1.0, 1.0, 1.0)
   love.graphics.rectangle('line', 0, 0, GAME_WIDTH, GAME_HEIGHT)
 
-  -- restore translation to state before window centering
+  -- score
+  love.graphics.setColor(1, 1, 1, 1)
+  love.graphics.print(tostring(num_enemies_cleared), 10, 10, 0, 2, 2)
+
+  -- restore translation to state before centering window
   love.graphics.pop()
 end
